d073698ad541ed8623f7c9db879db7cef06111e5
Vince <starryblue7@gmail.com>
2021-11-18 18:10:17 -0800

    adjust file paths for caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,8 @@
 const FILES_TO_CACHE = [
     "/",
     "/dist/manifest.json",
-    "/db.js",
-    "/index.js",
-    "/dist/db.bundle.js",
-    "/dist/index.bundle.js",
+    "./db.js",
+    "./index.js",
     "/dist/assets/icons/icon_192x192.png",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
