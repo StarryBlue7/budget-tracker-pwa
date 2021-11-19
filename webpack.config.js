02934e4a9b1b7973024ff6a8082c02fc924f9639
Vince <starryblue7@gmail.com>
2021-11-18 17:36:13 -0800

    remove bundling/minification interfering with indexedDB

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,29 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
 const config = {
-  mode: "development",
-  entry: "/public/index.js",
-  output: {
-    path: __dirname + "/public/dist",
-    filename: "bundle.js",
-    publicPath: "/dist",
-  },
-  plugins: [
-    new WebpackPwaManifest({
-      fingerprints: false,
-      inject: false,
-      name: "Budget Tracker App",
-      short_name: "Budget Tracker",
-      description: "An application for tracking budgets",
-      background_color: "#01579b",
-      theme_color: "#ffffff",
-      start_url: "/",
-      icons: [{
-        src: path.resolve("public/icons/icon-512x512.png"),
-        sizes: [96, 128, 192, 256, 384, 512],
-        destination: path.join("assets", "icons")
-      }]
-    })
-  ],
+    mode: "development",
+    entry: {},
+    output: {
+        path: __dirname + "/public/dist",
+        publicPath: "/dist",
+    },
+    plugins: [
+        new WebpackPwaManifest({
+            fingerprints: false,
+            inject: false,
+            name: "Budget Tracker App",
+            short_name: "Budget Tracker",
+            description: "An application for tracking budgets",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            start_url: "/",
+            icons: [{
+                src: path.resolve("public/icons/icon-512x512.png"),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join("assets", "icons")
+            }]
+        })
+    ],
 };
 
 module.exports = config;
